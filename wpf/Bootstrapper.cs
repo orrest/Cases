9583b1b31487e2c1225310575cb16941ff6275e0
feng <zzhxufeng@163.com>
2024-07-22 09:02:30 +0800

    添加项目文件。

---
+++ wpf/Bootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System.Windows;
+
+namespace wpf;
+
+internal class Bootstrapper : PrismBootstrapper
+{
+    protected override DependencyObject CreateShell()
+    {
+        return Container.Resolve<MainWindow>();
+    }
+
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+
+    }
+}
